78592e575d609a3b7e002fbb5d3b6ed7d2400c8d
Bruno Trevelin <brunonunest@pc-2-95.customer.ask4.lan>
2023-12-03 20:47:22 +0000

    fixedidNft

--- wellnftid.sol
+++ wellnftid.sol
@@ -1,61 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.5.0/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.5.0/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.5.0/contracts/utils/Strings.sol";
-
-
-// Main contract definition, inheriting from ERC721 for NFT functionality and Ownable for ownership control
 contract IdentityNFT is ERC721, Ownable {
-    using Strings for uint256; // Utilizing the Strings library for string conversion functions
+    using Strings for uint256;
 
-    // State variables
-    string private baseURI; // Base URI for token metadata
-    mapping(address => uint256) public userToIdentityToken; // Mapping from user addresses to their identity token IDs
-    mapping(uint256 => string) private tokenURIs; // Mapping from token IDs to their metadata URIs
-    uint256 private _tokenIdCounter = 0; // Counter to keep track of the last token ID issued
+    string private baseURI;
+    mapping(address => uint256) public userToIdentityToken;
+    uint256 private _tokenIdCounter = 0;
 
-    // Event emitted when a new identity NFT is minted
     event IdentityNFTMinted(address indexed user, uint256 tokenId);
 
-    // Constructor to initialize the contract with a given base URI
-    constructor(string memory _baseURI) ERC721("WIdentityNFT", "WIDNFT") {
-        baseURI = _baseURI;
+    constructor(string memory _baseURI) ERC721("WIdentityNFT", "WIDNFT") Ownable(msg.sender) {
+    baseURI = _baseURI;
     }
 
-    // Function to mint a new identity token for a user
     function mintIdentityToken(address user) external onlyOwner {
         require(userToIdentityToken[user] == 0, "User already has an identity token");
+        uint256 newTokenId = _tokenIdCounter + 1;
+        _tokenIdCounter = newTokenId;
 
-        uint256 newTokenId = _tokenIdCounter + 1; // Incrementing the token ID counter to get a new unique token ID
-        _tokenIdCounter = newTokenId; // Updating the counter
+        _mint(user, newTokenId);
+        userToIdentityToken[user] = newTokenId;
 
-        _mint(user, newTokenId); // Minting the token to the specified user
-        userToIdentityToken[user] = newTokenId; // Mapping the user address to the new token ID
-        
-        string memory newTokenURI = string(abi.encodePacked(baseURI, newTokenId.toString())); // Constructing the token URI
-        tokenURIs[newTokenId] = newTokenURI; // Associating the token ID with its URI
-
-        emit IdentityNFTMinted(user, newTokenId); // Emitting the mint event
+        emit IdentityNFTMinted(user, newTokenId);
     }
 
-    // Override for the tokenURI function to return the URI for a given token ID
+    // Updated tokenURI function
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return tokenURIs[tokenId];
+        require(tokenId > 0 && tokenId <= _tokenIdCounter, "ERC721Metadata: URI query for nonexistent token");
+
+        // Construct the token URI on the fly
+        return string(abi.encodePacked(baseURI, tokenId.toString()));
     }
 
-    // Function to update the base URI for metadata, callable only by the contract owner
     function setBaseURI(string calldata _baseURI) external onlyOwner {
         baseURI = _baseURI;
     }
 
-    // Optional function to check if a specific user has an identity token
     function hasIdentityToken(address user) external view returns (bool) {
         return userToIdentityToken[user] != 0;
     }
